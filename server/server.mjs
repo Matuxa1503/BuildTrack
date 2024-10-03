4eef7c89fea2deaccaf27766c4fb4690e05cf7d4
Matuxa1503 <muravskiy.03@mail.ru>
2024-10-03 18:27:28 +0300

    feat: add tg.mjs. Update server.mjs. Get data in App.jsx

--- server/server.mjs
+++ server/server.mjs
@@ -9,76 +9,21 @@ const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 app.get('/', async (req, res) => {
   try {
     const response = await axios.get('http://www.ghb.by/ru/construction/price_apartments/');
     const dataBuild = parseHtml(response.data);
-    // const dataBuild = [
-    //   {
-    //     title: '«Микрорайон многоквартирной жилой застройки в районе улиц Калиновского – Весенней в г. Гродно. Жилой дом № 14»',
-    //     dateBuildText: 'Дата начала продаж 13.09.2024г. ',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6957/',
-    //   },
-    //   {
-    //     title: ' «Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 12»',
-    //     dateBuildText: 'Дата начала продаж 03.09.2024г. ',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6840/',
-    //   },
-    //   {
-    //     title: '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 13»',
-    //     dateBuildText: 'Дата начала продаж 08.07.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6484/',
-    //   },
-    //   {
-    //     title: '«Жилой дом № 17 в квартале «Грандичи-4» в г. Гродно»',
-    //     dateBuildText: 'Дата начала продаж 28.06.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6460/',
-    //   },
-    //   {
-    //     title: '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 14»',
-    //     dateBuildText: 'Дата начала продаж 04.06.2024г. ',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6396/',
-    //   },
-    //   {
-    //     title:
-    //       '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 6 со встроенной детской амбулаторией на 70 посещений в смену» ',
-    //     dateBuildText: 'Дата начала продаж 28.05.2024г. ',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6356/',
-    //   },
-    //   {
-    //     title: '«Жилой дом № 16 в квартале «Грандичи-4» в г. Гродно»',
-    //     dateBuildText: 'Дата начала продаж 10.05.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6200/',
-    //   },
-    //   {
-    //     title: '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 5»',
-    //     dateBuildText: 'Дата начала продаж 03.05.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/6172/',
-    //   },
-    //   {
-    //     title: '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 9 со встроенными нежилыми помещениями»',
-    //     dateBuildText: 'Дата начала продаж 09.01.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/5775/',
-    //   },
-    //   {
-    //     title: '«Застройка территории в районе улиц Соломовой, Хвойной и Фолюш в г. Гродно. Жилой дом № 10»',
-    //     dateBuildText: 'Дата начала продаж 09.01.2024г.',
-    //     link: 'http://www.ghb.by/ru/construction/nedvizhimost-dogovor/5796/',
-    //   },
-    //   {
-    //     title: '«Застройка в г. Минск. Жилой дом № 1»',
-    //     dateBuildText: 'Дата начала продаж 01.01.2003г.',
-    //     link: 'http://localhost:3000/',
-    //   },
-    // ];
+
+    // данные текстового файла data.txt
     let dataFileTxt = '';
 
-    // Если данные пришли с сервера, проверяем пустой у нас data.txt. Если пустой закидываем данные из dataBuild,
+    // Если данные пришли с сервера, проверяем пустой ли у нас data.txt. Если пустой закидываем данные из dataBuild,
     // в противном случае забираем данные с data.txt и сравниваем с данными сервера. Достаем новые элементы если они есть в getNewElementsBuild
     if (dataBuild) {
-      dataFileTxt = JSON.parse(await getDataFile(path.join(__dirname, 'src', 'data.txt')));
+      const dataFile = await getDataFile(path.join(__dirname, 'src', 'data.txt'));
+      dataFileTxt = dataFile ? JSON.parse(dataFile) : '';
 
       if (dataFileTxt === '') {
         writeDataFile(dataBuild);
@@ -89,7 +34,20 @@ app.get('/', async (req, res) => {
       }
     }
   } catch (err) {
+    console.log(err);
     res.status(400).send('Not Found');
+  }
+});
+
+app.get('/last-element', async (req, res) => {
+  try {
+    let lastElementData;
+    const getData = await getDataFile(path.join(__dirname, 'src', 'data.txt'));
+
+    lastElementData = getData ? JSON.parse(getData)[0] : '';
+    res.json({ message: lastElementData });
+  } catch (err) {
+    console.log(err);
   }
 });
 
