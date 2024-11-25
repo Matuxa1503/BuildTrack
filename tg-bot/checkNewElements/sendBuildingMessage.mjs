4d434579402c5bc49d9a443daf27d74533ee7479
Matuxa1503 <muravskiy.03@mail.ru>
2024-11-25 16:52:35 +0300

    add file btnOptions

--- tg-bot/checkNewElements/sendBuildingMessage.mjs
+++ tg-bot/checkNewElements/sendBuildingMessage.mjs
@@ -1,15 +1,7 @@
+import btnOptions from '../btnOptions.mjs';
 import handle403Error from '../handle403Error.mjs';
 
 const sendBuildingMessage = async (bot, chatId, userId, item) => {
-  const linkItem = encodeURIComponent(item.data.link);
-  const user = encodeURIComponent(userId);
-
-  const btnOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [[{ text: 'Отобразить в реакт', url: `http://localhost:3000?link=${linkItem}&user=${user}` }]],
-    }),
-  };
-
   return bot
     .sendMessage(
       chatId,
--- tg-bot/getLastElement.mjs
+++ tg-bot/getLastElement.mjs
@@ -1,27 +1,18 @@
 import axios from 'axios';
+import btnOptions from './btnOptions.mjs';
 
 const getLastElement = async (bot, chat, userId) => {
   try {
     const chatId = chat;
     const response = await axios.get('http://localhost:5000/last', { params: { userId } });
-
     // если response.data.message пустой записать в el = null, в противном случае записать элемент
     const el = response.data.message ? response.data.message.items[0].data : null;
 
     if (el) {
-      const linkItem = encodeURIComponent(el.link);
-      const user = encodeURIComponent(userId);
-
-      const btnOptions = {
-        reply_markup: JSON.stringify({
-          inline_keyboard: [[{ text: 'Отобразить информацию в React приложении', url: `http://localhost:3000/?link=${linkItem}&user=${user}` }]],
-        }),
-      };
-
       return bot.sendMessage(
         chatId,
         `Последняя застройка: \n${el.title}.\n${el.dateBuild} \nДля подробной информации кликнете по кнопке ниже:`,
-        btnOptions
+        btnOptions(el.link, userId)
       );
     } else {
       return bot.sendMessage(chatId, `Ошибка при выполнении команды. Попробуйте позже`);
