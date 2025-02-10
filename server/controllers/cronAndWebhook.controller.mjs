93ae33fd3cedb646883a1db9afa6a84bc9b5ac45
Matuxa1503 <muravskiy.03@mail.ru>
2025-02-10 19:26:25 +0300

    update controller

--- server/controllers/cronAndWebhook.controller.mjs
+++ server/controllers/cronAndWebhook.controller.mjs
@@ -10,17 +10,18 @@ export const runCronJob = async () => {
 
 export const handleWebhookTg = async (req, res) => {
   try {
+    console.log(req.body);
     if (req.body.message && req.body.message.text && req.body.message.chat && req.body.message.from) {
       const body = req.body.message;
       console.log('message', body);
       await handleCommandTg(body.text, body.chat.id, body.from.id);
     }
 
-    if (req.body.callback_query && req.body.callback_query.data) {
-      const body = req.body.callback_query;
-      console.log('callback_query', body);
-      await handleCommandTg(body.data, body.message.chat.id, body.from.id);
-    }
+    // if (req.body.callback_query && req.body.callback_query.data) {
+    //   const body = req.body.callback_query;
+    //   console.log('callback_query', body);
+    //   await handleCommandTg(body.data, body.message.chat.id, body.from.id);
+    // }
 
     res.status(200).send('ok');
   } catch (err) {
